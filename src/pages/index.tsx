7b40c5d17f5864898d80b28c06d76ffb60c7cae8
thalha <thalhaz999@gmail.com>
2023-11-08 16:33:10 +0530

    feat:function name updated

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,5 @@
 import { Inter } from 'next/font/google'
-import Form from '../components/Form/Form'
 import Add from '../components/add/Add'
-import TestCase from '../components/add/TestCase'
 
 const inter = Inter({ subsets: ['latin'] })
 
